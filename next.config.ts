1e38f15350ad320d2d2d62d19f853e4c5308075e
Jaehoon Hwang <jaehoon217@gmail.com>
2025-05-23 00:19:34 -0700

    Create out directory witht the upload artifact step

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/volleyball-matcher",
+  basePath: process.env.NODE_ENV === "production" ? "/volleyball-matcher" : "",
+  images: {
+    unoptimized: true,
+  },
   output: "export",
   reactStrictMode: true,
 };
