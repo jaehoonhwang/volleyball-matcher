7923ba6c2ce3cbcc84e58d106682cce55df2f2af
Jaehoon Hwang <jaehoon217@gmail.com>
2025-05-26 01:46:43 -0700

    Remove image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-4">
-          🚀 Landing page for the site 🚀{" "}
+          🚀 Landing page for the site 🚀
         </h1>
         <p className="text-lg text-gray-700 mb-8">
-          Where we get tinder'd into volleyball matches.
+          {"Where we get tinder'd into volleyball matches."}
         </p>
         <p className="text-md text-gray-600">
           World of getting disappointed/happy at the same time with your
