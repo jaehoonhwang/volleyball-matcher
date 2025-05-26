e5cf84775e17f1052c402665dff8703b9651edd0
Jaehoon Hwang <jaehoon217@gmail.com>
2025-05-26 00:54:01 -0700

    copy stuff from nextjs deploy-github-pages

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/volleyball-matcher",
+  basePath: process.env.PAGES_BASE_PATH,
   output: "export",
   reactStrictMode: true,
 };
